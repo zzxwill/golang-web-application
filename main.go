4aae5058195a1fd0e88480089c2cacd514d993fe
zzxwill <zzxwill@gmail.com>
2021-01-31 22:17:36 +0800

    add .gitignore

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    fmt.Println("Starting...")
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
